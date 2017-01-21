935a10b0427a3eedeac17c440a81c72efe7fe389
jonas <jonas8@me.com>
2017-01-21 22:46:39 +0800

    add ejs include files to webpack dependency

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ var ejs = require('ejs'),
   path = require('path')
 
 module.exports = function(source) {
-  this.cacheable && this.cacheable();
+  this.cacheable && this.cacheable()  
   var query = loaderUtils.parseQuery(this.query)
-  var options = this.options.__ejsOptions__ = Object.assign({}, this.options.ejs, this.ejs, query)
-  options.filename = path.relative(process.cwd(), this.resourcePath);
+  var filename = path.relative(process.cwd(), this.resourcePath)
+  var includeReg = /\s*include[\('\s]*(\S+)'/g, match
+  while (match = includeReg.exec(source)) {
+    this.dependency(path.normalize(ejs.resolveInclude(match[1], filename)));
+  }
+  var defaultOptions = {
+    compileDebug: true,
+    filename: filename
+  }
+  var options = this.options.__ejsOptions__ = Object.assign(defaultOptions, this.options.ejs, this.ejs, query)
   var compiler = ejs.compile(source, options)
   return 'module.exports = ' + (
     options.client === true
