e2c002465e2d8e75f6e4def4fc485de160fa58c3
jonas <jonas8@me.com>
2017-01-21 18:45:31 +0800

    version change

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ var ejs = require('ejs'),
 module.exports = function(source) {
   this.cacheable && this.cacheable();
   var query = loaderUtils.parseQuery(this.query)
-  var options = this.options.__vueOptions__ = Object.assign({}, this.options.ejs, this.ejs, query)
+  var options = this.options.__ejsOptions__ = Object.assign({}, this.options.ejs, this.ejs, query)
   options.filename = path.relative(process.cwd(), this.resourcePath);
-  options.debug=false
   var compiler = ejs.compile(source, options)
   return 'module.exports = ' + (
     options.client === true
